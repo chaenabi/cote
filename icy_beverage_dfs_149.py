ed29c4cabcc5935e53ddd545966946cef132fe7b
chaenabi <find1086@gmail.com>
2021-07-15 11:23:09 +0900

    fix solve

--- icy_beverage_dfs_149.py
+++ icy_beverage_dfs_149.py
@@ -5,7 +5,7 @@ for i in range(n):
     graph.append(list(map(int, input())))
 
 def dfs(x, y):
-    if not -1 <= x <= n and not -1 <= y <= m:
+    if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
     if graph[x][y] == 0:
         graph[x][y] = 1
